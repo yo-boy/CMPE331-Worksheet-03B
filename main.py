68b97fe11bdd109aaff3ca3975bfbfdfea2c66af
yo_boi <abdelrahmanbbb@gmail.com>
2022-12-02 11:30:05 +0300

    imported beutiful soup and added a main function and a parse function

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ Stage : Development -01
  @author : Hamza Sallam , 120200013
 
 """
-
+from bs4 import BeautifulSoup
 import urllib.request
 import sys
 # we take arguments in the form "2022 12 2 climate"
@@ -15,3 +15,11 @@ def getCategory(category):
     # returns the html file of category as a string
     return urllib.request.urlopen("https://www.nytimes.com/section/"+ category).read()
 
+def parseDate(html, date):
+    soup = BeautifulSoup(html, 'html.parser')
+    
+def main(args):
+    # takes a date and category as command line arguments and return an array of headlines
+    html = getCategory(args[-1])
+    date = args[0] + "/" + args[1] + "/" + args[2]
+    return parseDate(html, date)
