70b5b413954eee4008043d1f696944dd9f4e5173
yo_boi <abdelrahmanbbb@gmail.com>
2022-12-02 11:16:29 +0300

    added a function to get the html file from a category

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 """
 Stage : Development -01
  @author : Abdel Rahman Abdin , 120200128
- @author : 
+ @author : Hamza Sallam , 120200013
 
 """
+
+import urllib.request
+import sys
+# we take arguments in the form "2022 12 2 climate"
+arguments = sys.argv
+
+
+def getCategory(category):
+    # returns the html file of category as a string
+    return urllib.request.urlopen("https://www.nytimes.com/section/"+ category).read()
+
